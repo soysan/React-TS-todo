97128be6305ec1d617250ad64654a473861b6e50
Masayuki <soy2.san@gmail.com>
2021-06-13 18:35:21 +0900

    made models of todo refacterd App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Todos from './components/Todos';
+import Todo from "./models/todo";
 import './App.css';
 
 function App() {
+  const todos = [new Todo('learn react'), new Todo('learn typescript')];
+
   return (
     <div>
-      <Todos items={['react', 'typescript']} />
+      <Todos items={todos} />
     </div>
   );
 }
